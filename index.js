8313f215003feb0a4bd61bd47d06f8d7b72d953f
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-20 16:40:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ configurationObject = {
   headers: {
     "Content-Type": "application/json"
      "Accept": "application/json"
-  }
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
 };
 
 fetch(destinationURL, configurationObject);
