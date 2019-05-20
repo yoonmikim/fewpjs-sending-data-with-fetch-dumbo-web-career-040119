abd53e2c10129ca73652d884026bf19aa0757876
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-20 16:45:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your code here
 function submitData(name, email){
-  return fetch("http://localhost:3000/dogs", {
+  return fetch("http://localhost:3000/users", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
