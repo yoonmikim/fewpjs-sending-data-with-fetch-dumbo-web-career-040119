faa0fb2fb4093d29cfd4e366fb17c9d43ffb85b0
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-20 16:41:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ fetch("http://localhost:3000/dogs", {
     dogName: "Byron",
     dogBreed: "Poodle"
   })
-});
+}).then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
